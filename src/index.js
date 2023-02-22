9a749c5e524919b71fbd180e8b4a23069c79a636
ali-khoshqamat <ali_khoshghamat@yahoo.com>
2023-02-22 07:43:10 +0330

    feat ButtonComponent

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import Button from "./Button";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Button />);
-
-export { default as Button } from "./Button";
